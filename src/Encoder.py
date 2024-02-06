d8d1abc9e5e1107060448a57d3ac948773d1d850
Mioshan <147013512+Mioshan@users.noreply.github.com>
2024-02-06 14:46:50 -0800

    Deoxygen Documentation.
    Added Deoxygen comments to Encoder class and its associated functions.

--- src/Encoder.py
+++ src/Encoder.py
@@ -1,11 +1,29 @@
+"""! @file Encoder.py
+This program is an Encoder reading class where the program is able
+to read a 12V Brushed DC motor using a L6206 H-Bridge Motor Driver.
+The class initializes the Encoder's pins and Timer to read the Motor's
+position.
+"""
 import pyb
 import utime
 
 class Encoder:
-    
+    """!
+    This class implements a L6206 H-Bridge Motor Controller Shield.
+    """
     
     def __init__ (self, ENA, ENB, timer, CHA, CHB):
-        
+        """!
+        Creates an Encoder class that intializes the Encoder's
+        pins and sets the set GPIO Pin Timer and respective channels.
+        @param ENA GPIO pin associated with Encoder Channel A.
+        @param ENB GPIO pin associated with Encoder Channel B.
+        @param timer Timer for the Encoder.
+        @param CHA Timer Channel corresponding to Encoder Channel A.
+        @param CHB Timer Channel corresponding to Encoder Channel B.
+        @param lastCount Most recent Encoder count.
+        @param totalCount Total Encoder count.
+        """
         self.pinENA = pyb.Pin(ENA, pyb.Pin.IN)
         self.pinENB = pyb.Pin(ENB, pyb.Pin.IN)
         
@@ -16,6 +34,13 @@ class Encoder:
         self.totalCount = 0
         
     def read(self):
+        """!
+        Function reads the Encoder's position as a positive/negative integer.
+        Accounts for under- and over-flow cases by comparing the most recent
+        and current count from the Encoder.
+        @param count Current Encoder count.
+        @param d Changes from last Encoder Count to the most recent Encoder count.
+        """
         count = self.tim.counter()
         d = count-self.lastCount
         if d >= 32768:
@@ -30,6 +55,9 @@ class Encoder:
         
     
     def zero(self):
+        """!
+        Function set all Encoder counts to 0.
+        """
         self.tim.counter(0)
         self.totalCount = 0
         self.lastCount = 0
