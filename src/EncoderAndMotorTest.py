b0f1f5dada72cc0e79e8e6f0a89e62cc33355a3a
logdotzipp <loganfemling@gmail.com>
2024-02-10 14:39:10 -0800

    Renamed file and updated documentation

--- src/EncoderAndMotorTest.py
+++ src/EncoderAndMotorTest.py
@@ -1,6 +1,6 @@
-"""! @file EncoderTest.py
+"""! @file EncoderAndMotorTest.py
 This program tests the Encoder class by running a DC motor at a speed and reading the encoder count.
-Also makes use of the MotorDriver class
+The motor is run with a Motor Driver class.
 """
 import pyb
 import micropython
@@ -11,15 +11,20 @@ import utime
 if __name__ == "__main__":
     
     try:
+        # Setup Motor Object
         motor1 = MotorDriver(pyb.Pin.board.PA10, pyb.Pin.board.PB4, pyb.Pin.board.PB5, pyb.Timer(3, freq=20000))
+        # Setup Encoder Object
         coder = Encoder(pyb.Pin.board.PB6, pyb.Pin.board.PB7, 4, 1, 2)
+        
         while True:
+            # Run Motor
             motor1.set_duty_cycle(-40)
             utime.sleep(1)
             while True:
+                # Read the encoder value and print it
                 print(coder.read())
+                # Pause so as not to overrun console
                 utime.sleep(.1)
-
 
     except(KeyboardInterrupt):
         print("Program Terminated")
